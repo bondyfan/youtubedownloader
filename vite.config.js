e1ed15855faac1c1dfb6c536d115598ef4581a06
Frank Wild <bondyfan@me.com>
2026-03-05 15:12:35 +0100

    fix: output vite build to root dist for netlify

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   root: 'public',
   publicDir: false,
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true
+  },
   server: {
     host: true,
     port: 5173,
